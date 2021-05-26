cbe8c397c0ae5275e09d9862e2031604fe4326aa
Tornike Goshadze <joffarex@gmail.com>
2021-05-27 00:26:56 +0400

    Setup base KXLogger

---
+++ KernX.Logger/KXLoggerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace KernX.Logger
+{
+    public static class KXLoggerExtensions
+    {
+        // Make sure not to break in this function but one level higher during DEBUG
+        [Conditional("DEBUG")]
+        public static void Assert<T>(this ILogger<T> logger, bool condition, string message)
+        {
+            if (!condition)
+            {
+                logger.LogError("Assertion Failed: {Message}", message);
+                if (Debugger.IsAttached)
+                {
+                    logger.LogError(new StackTrace().ToString());
+                    Debugger.Break();
+                }
+            }
+        }
+    }
+}
---
+++ KernX.Logger/ThreadEnricher.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace KernX.Logger
+{
+    public class ThreadEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(
+                propertyFactory.CreateProperty("TheadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+}
