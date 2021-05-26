4392433b5a3e2680ac3be3906a49afba8f4804df
Tornike Goshadze <joffarex@gmail.com>
2021-05-26 23:57:24 +0400

    Initialize DAL

---
+++ KernX.DAL/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KernX.DAL
+{
+    public abstract record BaseEntity
+    {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ KernX.DAL/IAsyncRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace KernX.DAL
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByConditionAsync(Expression<Func<T, bool>> filter);
+        Task AddAsync(T model);
+        Task UpdateAsync(T model);
+        Task UpdateByIdAsync(Guid id);
+        Task BatchUpdateAsync(IEnumerable<T> entities);
+        Task RemoveAsync(T model);
+        Task RemoveByIdAsync(Guid id);
+        Task BatchRemoveAsync(IEnumerable<T> entities);
+    }
+}
