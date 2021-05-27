3c864fa12744452a57c62c0748c06e14405bd9c4
Tornike Goshadze <joffarex@gmail.com>
2021-05-27 22:19:35 +0400

    Update EventBus lib

--- KernX.EventBus/IEventBus.cs
+++ KernX.EventBus/IEventBus.cs
@@ -5,7 +5,7 @@ namespace KernX.EventBus
 {
     public interface IEventBus
     {
-        void Publish<T>(string exchange, string appId, T @event) where T : IEvent;
-        void Subscribe<T>(string queueName, Func<EventHeaders, T, Task> callback);
+        Task Publish<T>(string topic, string appId, T @event) where T : IEvent;
+        Task Subscribe<T>(string queue, Func<EventHeaders, T, Task> callback);
     }
 }
