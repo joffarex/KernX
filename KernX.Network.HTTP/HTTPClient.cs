773af2483eb8ad774ea6e9b0972319470ff5c248
Tornike Goshadze <joffarex@gmail.com>
2021-05-27 18:29:59 +0400

    Introduce HTTPClient Logger

--- KernX.Network.HTTP/HTTPClient.cs
+++ KernX.Network.HTTP/HTTPClient.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using KernX.Logger;
+using Microsoft.Extensions.Logging;
 
 namespace KernX.Network.HTTP
 {
@@ -10,6 +12,9 @@ namespace KernX.Network.HTTP
     {
         private static readonly HttpClient Client = new();
         private readonly string _baseURL;
+        private readonly ILogger<HTTPClient> _logger;
+
+        public HTTPClient(ILogger<HTTPClient> logger) => _logger = logger ?? KXLogger.CreateLogger<HTTPClient>();
 
         public string BaseURL
         {
@@ -30,13 +35,16 @@ namespace KernX.Network.HTTP
         public string UserAgent { get; init; }
         public Dictionary<string, string> SharedHeaders { get; init; } = null;
 
-
         public async Task<HTTPResponse<TResponse>> GetRequestAsync<TResponse>(HTTPRequest request)
         {
             SetupHeaders(request);
 
             string queryString = HTTPClientHelpers.GenerateQueryString(request.QueryParams);
-            HttpResponseMessage response = await Client.GetAsync($"{BaseURL}{request.Endpoint}{queryString}");
+
+            var url = $"{BaseURL}{request.Endpoint}{queryString}";
+            _logger.LogInformation($"Sending - GET {url}");
+            HttpResponseMessage response = await Client.GetAsync(url);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
 
             return await HTTPResponse<TResponse>.CreateFromClient(response);
         }
@@ -45,7 +53,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.PostAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - POST {url}");
+
+            HttpResponseMessage response = await Client.PostAsync(url, request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return HTTPResponse.CreateFromClient(response);
         }
 
@@ -53,7 +66,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.PostAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - POST {url}");
+
+            HttpResponseMessage response = await Client.PostAsync(url, request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return await HTTPResponse<TResponse>.CreateFromClient(response);
         }
 
@@ -61,7 +79,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.PatchAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - PATCH {url}");
+
+            HttpResponseMessage response = await Client.PatchAsync(url, request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return HTTPResponse.CreateFromClient(response);
         }
 
@@ -70,7 +93,11 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - PATCH {url}");
+
             HttpResponseMessage response = await Client.PatchAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
 
             return await HTTPResponse<TResponse>.CreateFromClient(response);
         }
@@ -79,7 +106,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.PutAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - PUT {url}");
+
+            HttpResponseMessage response = await Client.PutAsync(url, request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return HTTPResponse.CreateFromClient(response);
         }
 
@@ -88,7 +120,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.PutAsync($"{BaseURL}{request.Endpoint}", request.Body);
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - PUT {url}");
+
+            HttpResponseMessage response = await Client.PutAsync(url, request.Body);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return await HTTPResponse<TResponse>.CreateFromClient(response);
         }
 
@@ -96,7 +133,12 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.DeleteAsync($"{BaseURL}{request.Endpoint}");
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - DELETE {url}");
+
+            HttpResponseMessage response = await Client.DeleteAsync(url);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return HTTPResponse.CreateFromClient(response);
         }
 
@@ -105,17 +147,24 @@ namespace KernX.Network.HTTP
         {
             SetupHeaders(request);
 
-            HttpResponseMessage response = await Client.DeleteAsync($"{BaseURL}{request.Endpoint}");
+            var url = $"{BaseURL}{request.Endpoint}";
+            _logger.LogInformation($"Sending - DELETE {url}");
+
+            HttpResponseMessage response = await Client.DeleteAsync(url);
+            _logger.LogInformation($"Native HttpClient response\n{response}");
+
             return await HTTPResponse<TResponse>.CreateFromClient(response);
         }
 
         private void SetupHeaders(HTTPRequest request)
         {
+            _logger.LogInformation("Setting up headers");
             Client.DefaultRequestHeaders.Accept.Clear();
             Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(request.ContentType));
             Client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
             HTTPClientHelpers.AddHeaders(Client, SharedHeaders);
             HTTPClientHelpers.AddHeaders(Client, request.Headers);
+            _logger.LogInformation($"Headers List\n{Client.DefaultRequestHeaders}");
         }
     }
 }
